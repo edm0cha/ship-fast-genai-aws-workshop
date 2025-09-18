974ca8ee56289048bfbb8b710f57130ddc070f1b
Edwin Moedano <edwin.moedano@caylent.com>
2025-09-18 17:24:00 -0600

    feat: added backend to handle the state

---
+++ infrastructure/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "ship-fast-edwin-genai-workshop"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
